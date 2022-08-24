226beee716225f5c7acad2c0d309c44413a43faa
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-24 22:42:37 +0100

    Added JMS Message listener example

--- src/main/java/mrw007/springframework/springjms/models/HelloWorldMessage.java
+++ src/main/java/mrw007/springframework/springjms/models/HelloWorldMessage.java
@@ -1,9 +1,6 @@
 package mrw007.springframework.springjms.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.UUID;
