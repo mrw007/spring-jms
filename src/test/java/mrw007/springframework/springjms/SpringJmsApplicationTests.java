f2c542dc67286011b39a1efa381bc1739546f2de
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-24 21:47:08 +0100

    initial project add

---
+++ src/test/java/mrw007/springframework/springjms/SpringJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package mrw007.springframework.springjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
